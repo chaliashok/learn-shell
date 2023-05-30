c07182fb45d7e0eda78c2e0f6b4479a519d74fd0
chaliashok <chali.ashok@gmail.com>
2023-05-29 23:17:15 -0500

    Mediawiki

---
+++ mediawiki/mediawiki.sh
@@ -0,0 +1,9 @@
+cd /tmp
+url="https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz"
+curl -O $url
+folder=$(echo $url | awk -F / '{print $6}'|sed -i "s/.tar.gz//")
+yum install httpd -y
+rm -rf /var/www/html/*
+cd /var/www/html
+tar -xvzf /tmp/mediawiki-1.39.3.tar.gz
+mv $folder mediawiki
