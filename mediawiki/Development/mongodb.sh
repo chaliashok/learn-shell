06f76d3d206bc0c4884e03d788dd106189fa785b
chaliashok <chali.ashok@gmail.com>
2023-05-30 19:25:59 -0500

    roboshop project code testing

---
+++ mediawiki/Development/mongodb.sh
@@ -0,0 +1,7 @@
+cp /home/centos/learn-shell/mediawiki/Development/mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+
+systemctl restart mongod
---
+++ mediawiki/Development/shipping.sh
@@ -0,0 +1,21 @@
+yum install maven -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+cd /app
+unzip /tmp/shipping.zip
+cd /app
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+
+cp /home/centos/learn-shell/mediawiki/Development/shipping.service /etc/systemd/system/shipping.service
+
+systemctl daemon-reload
+
+systemctl enable shipping
+systemctl start shipping
+yum install mysql -y
+
+mysql -h mysql-dev.devopsawschinni.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+systemctl restart shipping
